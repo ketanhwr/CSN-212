2fda91863bf7df709ccc1eb915f8be77212dab90
Ketan Gupta <ketan19972010@gmail.com>
2017-02-16 22:43:21 +0530

    Adds prototype of functions

--- Tutorial-3/interval.cpp
+++ Tutorial-3/interval.cpp
@@ -11,3 +11,46 @@
 using namespace std;
 
 // Definition of the functions of Interval Class
+
+Interval::Interval()
+{
+	root = NULL;
+}
+
+void Interval::insert(int l, int r)
+{
+
+}
+
+void Interval::insert_helper(node *current, int l, int r)
+{
+
+}
+
+range Interval::search(int l, int r)
+{
+
+}
+
+range Interval::search_helper(node *current, int l, int r)
+{
+
+}
+
+void Interval::remove(int l, int r)
+{
+
+}
+
+void Interval::remove_helper(node *current, int l, int r)
+{
+
+}
+
+int main()
+{
+	//Code
+
+
+	return 0;
+}
--- Tutorial-3/interval.h
+++ Tutorial-3/interval.h
@@ -19,6 +19,15 @@ struct node {
 	// Pointer to left and right children
 	node *left;
 	node *right;
+
+	// Constructor of the node structure
+	node(int l, int r)
+	{
+		r = range(l, r);
+		max_right = r;
+
+		left = right = NULL;
+	}
 };
 
 // If class Interval is not defined then define its prototype
@@ -29,7 +38,30 @@ struct node {
 // The class for Interval Tree along with all the operations
 class Interval
 {
+	private:
+		node *root;
 
+	public:
+		// Constructor
+		Interval();
+
+		// Insert a range
+		void insert(int, int);
+
+		// Function called by insert() to recursively traverse and insert a range
+		void insert_helper(node *, int, int);
+
+		// Search for a range
+		range search(int, int);
+
+		// Function called by search() to search for a range
+		range search_helper(node *, int, int);
+
+		// Remove a range
+		void remove(int, int);
+
+		// Helper function to remove a range
+		void remove_helper(node *, int, int);
 
 };
 
